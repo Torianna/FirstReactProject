6cb6afa6ff7eb62bbe288fce89467afbdcfdfbf4
Klaudia <klaudia.goralska95@gmail.com>
2020-04-18 17:14:24 +0200

    Lifecycle of component after update props

--- src/components/Articles/Article/Article.js
+++ src/components/Articles/Article/Article.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Article.css';
 import styles from './Article.module.css';
 
-
-const article = (props) =>
+class Article extends Component
 {
-    console.log("articles");
+    
+    // static getDerivedStateFromProps(this.props,state){
+    //     //wywoływane na początku 
+    //     console.log("Articles GetDerivedStateFromProps");
+    //     return state;
+    // }
+
+    shouldComponentUpdate(nextprops, nextstate)
+    { //anulowanie aktualizacji komponetu
+        //druga?
+        //analizowanie propsów i state'ów
+        console.log("Articles shouldComponentUpdate");
+        //chce aktualizowac - true
+        return true;
+    }
+
+    getSnapshotBeforeUpdate(prevprops, prevstate)
+    {
+        //po render
+        //gdy chcemy pobrać położonie w jakim użytkownik znajdował się przed zaktualizwaniem komponentu
+        console.log("Articles getSnapshotBeforeUpdate");
+        return {message: " this is text message"};
+    }
+
+    componentDidUpdate(prevprops, prevstate, snapchot)
+    {
+        //po render
+        //inf ze aktualizacja się skończyła
+        //wywolywanie zapytan http
+        //asynchroniczne zmiany 
+        console.log("Articles componentDidUpdate ");
+        console.log("Snapchot message: "+ snapchot.message)
+    }
+
+    render(){
+
+
+    console.log("Articles render");
 
     const style={
         '@media (min-width: 900px)':{
@@ -16,13 +52,14 @@ const article = (props) =>
 return(
 /* <div className="Article"  style={style}> */
     <div className={styles.article}>
-    <h2>{props.title}</h2>
-    <p className={styles.violet}>{props.content}</p>
-    <button onClick={props.deleteClick}>Delete article</button>
+    <h2>{this.props.title}</h2>
+    <p className={styles.violet}>{this.props.content}</p>
+    <button onClick={this.props.deleteClick}>Delete article</button>
     <br></br>
     <br></br>
     </div>
 );
+    }
 }
 
-export default article;
+export default Article;
